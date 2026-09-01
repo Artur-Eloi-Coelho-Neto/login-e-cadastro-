c2cb12741cb087cb9b87ff216e11b95ca390a902
Artur-Eloi-Coelho-Neto <artureloiwork@gmail.com>
2026-09-01 12:12:09 -0300

    projeto tela login e cadastro com java

---
+++ tela-de-login/src/main/java/com/artur/tela_de_login/TelaDeLoginApplication.java
@@ -0,0 +1,14 @@
+package com.artur.tela_de_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelaDeLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelaDeLoginApplication.class, args);
+	}
+
+	
+}
---
+++ tela-de-login/src/test/java/com/artur/tela_de_login/TelaDeLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artur.tela_de_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelaDeLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
